e79eec2a30e43b6b69c48c5e0ea2f993fdf7e50d
julianabc <julianabc@id.uff.br>
2020-11-18 14:38:00 -0300

    Create cadastro-carro.component.ts

---
+++ src/app/pages/cadastro-carro/cadastro-carro.component.ts
@@ -0,0 +1,62 @@
+import { Car } from './../../models/car';
+import { Component, OnInit } from '@angular/core';
+import { CarService } from './../../services/car.service';
+import { NgForm } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-cadastro-carro',
+  templateUrl: './cadastro-carro.component.html',
+  styleUrls: ['./cadastro-carro.component.scss']
+})
+export class CadastroCarroComponent implements OnInit {
+
+  car = {} as Car;
+  cars: Car[];
+
+  constructor(private carService: CarService) {}
+
+  ngOnInit() {
+    this.getCars();
+  }
+
+  // defini se um carro será criado ou atualizado
+  saveCar(form: NgForm) {
+    if (this.car.id !== undefined) {
+      this.carService.updateCar(this.car).subscribe(() => {
+        this.cleanForm(form);
+      });
+    } else {
+      this.carService.saveCar(this.car).subscribe(() => {
+        this.cleanForm(form);
+      });
+    }
+  }
+
+  // Chama o serviço para obtém todos os carros
+  getCars() {
+    this.carService.getCars().subscribe((cars: Car[]) => {
+      this.cars = cars;
+    });
+  }
+
+  // deleta um carro
+  deleteCar(car: Car) {
+    this.carService.deleteCar(car).subscribe(() => {
+      this.getCars();
+    });
+  }
+
+  // copia o carro para ser editado.
+  editCar(car: Car) {
+    this.car = { ...car };
+  }
+
+  // limpa o formulario
+  cleanForm(form: NgForm) {
+    this.getCars();
+    form.resetForm();
+    this.car = {} as Car;
+  }
+
+}

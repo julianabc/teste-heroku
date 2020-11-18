c56ff175964e804b74737d74cb2593af567f2332
julianabc <julianabc@id.uff.br>
2020-11-18 14:38:18 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
-  title = 'teste-solidarize';
-
+export class AppComponent{
 
 }
